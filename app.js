c47ad2c2711bc9d419f1ac252402cbdf0d7ce29d
sanjaymajhi <1sanjaymajhi@gmail.com>
2020-04-29 02:04:09 +0530

    test build

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users/", usersRouter);
-
+app.use(express.static(path.join(__dirname, "build")));
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
