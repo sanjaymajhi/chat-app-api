ab29fc338ddf069b72275628622ed1343b6528c4
sanjaymajhi <1sanjaymajhi@gmail.com>
2020-05-06 19:57:59 +0530

    some small changes

---
+++ build/precache-manifest.194be7fd337b01318aa26e77a14678b9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbdb0c6c4ba29dae43ab59fdecebd1de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6cff659e5cab81f41f8e",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "40919ddfe816ac639a97",
+    "url": "/static/css/main.0aed37c8.chunk.css"
+  },
+  {
+    "revision": "6cff659e5cab81f41f8e",
+    "url": "/static/js/2.829574fb.chunk.js"
+  },
+  {
+    "revision": "f576af3254df0679363d3e6ffe804e36",
+    "url": "/static/js/2.829574fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40919ddfe816ac639a97",
+    "url": "/static/js/main.db0ba86d.chunk.js"
+  },
+  {
+    "revision": "bcaab4293dc7b84d6998",
+    "url": "/static/js/runtime-main.343aded6.js"
+  },
+  {
+    "revision": "1e976c46d0c8de419b7645c0a1835d7c",
+    "url": "/static/media/avtar.1e976c46.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1762536111107c82552957b4fe61d54f.js"
+  "/precache-manifest.194be7fd337b01318aa26e77a14678b9.js"
 );
 
 self.addEventListener('message', (event) => {
