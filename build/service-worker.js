8c1b26c23c13c0adc68c7d76686dbb3743a2f443
sanjaymajhi <1sanjaymajhi@gmail.com>
2020-05-13 12:15:18 +0530

    some changes

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8b7722472e980cbde1e657687deb3a0d.js"
+  "/precache-manifest.6d0d80e33fbed4d0517d7b2b4f201a23.js"
 );
 
 self.addEventListener('message', (event) => {
